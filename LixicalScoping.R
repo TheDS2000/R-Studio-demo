69374a1ecbaabf2450f3d43802cbd6ed47ba51be
Dhanashri_Shinde <50336672+TheDS2000@users.noreply.github.com>
2020-05-29 00:34:22 -0400

    Update LixicalScoping.R

--- LixicalScoping.R
+++ LixicalScoping.R
@@ -1,4 +1,7 @@
 
+## The first function name is makeCacheMatrix.
+
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -12,8 +15,9 @@ makeCacheMatrix <- function(x = matrix()) {
        setmatrix = setmatrix,
        getmatrix = getmatrix)
 }
-
-## Return a matrix that is the inverse of 'x'
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the
+## matrix has not changed), then it should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
   m <- x$getmatrix()
   if(!is.null(m)) {
